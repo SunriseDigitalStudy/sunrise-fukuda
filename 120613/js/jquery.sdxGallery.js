13444ce1e5e295f5e623e65ddae6115d162df070
sunrise-fukuda <fukuda@sunrisedigital.jp>
2013-01-18 13:51:04 +0900

    修正

--- 120613/js/jquery.sdxGallery.js
+++ 120613/js/jquery.sdxGallery.js
@@ -3,38 +3,43 @@ $.fn.gallery = function(option) {
 		   var settings = $.extend({
 					right: null,
 					left: null,
-					thum: null
+					thum: null,
+					onInit: null,
+					onChange: null
 	       }, option);
 		   
 		   var liNum = $(this).children("li").length;		   // liの数取得
 		   var liWidth = $(this).children("li").width();	   // liの幅取得
 		   var ulWidth = liWidth*liNum;		  		   // ulの幅取得
 		   var moveLimit = ulWidth-liWidth;			   // ulの移動制限距離
-		   $(this).width(ulWidth);		   		   // #bigImgの幅決定
+		   $(this).width(ulWidth);		   		   // $(this)の幅決定
+		   var changeImg = $(this);			// $(this)を変数に格納
+
 		   // ナンバリング用画像初期番号
 		   var imageNum = 1;
 		   
 		   //　初期の画像枚数表示
-		   $("span.current").text(1);
-		   $("span.total").text(liNum);
+			var mLeft = $(this).css("margin-left");
+			var difference = moveLimit+parseInt(mLeft);
+			var nowNumber = 1+parseInt(mLeft)/-liWidth;
+			settings.onChange(nowNumber);
+			settings.onInit(liNum);
 		   
 			// 画像ナンバリング
 			$(this).find("img").each(function(){
 				var detaNum = imageNum++;
 				$(this).attr("deta-img",detaNum);
 				if ($(this).is("li:last-child img")){
-				return this;
 				}
 			});
 			
 			// 大画像のデータからサムネイル表示
 				var thumDate = $(this).html();
 				$(settings.thum).html(thumDate);
-				$(settings.thum).find("li:first-child img").addClass("select"); //初期選択状態
+				$(settings.thum).find("li:nth-child("+ nowNumber +") img").addClass("select"); //初期選択状態
 				if ($(this).is("li:last-child img")){
-				return this;
 				}
-		
+				
 		function changeTo(direction){
 			   		// 左右とサムネクリックでの動作判定
 					if (direction == 0){
@@ -47,9 +52,8 @@ $.fn.gallery = function(option) {
 						var plusMinus = "-";
 					}
 					// 動作開始
-					console.log(this)
-					$("#bigImg:not(:animated)").animate({marginLeft: plusMinus + (direction * liWidth - liWidth) + "px"},
-						   {duration:"nomal",
+					$(changeImg).not(":animated").animate({marginLeft: plusMinus + (direction * liWidth - liWidth) + "px"},
+						   {duration:"fast",
 							complete:function(){
 								//　ボタンの表示非表示
 								var mLeft = $(this).css("margin-left");
@@ -69,14 +73,14 @@ $.fn.gallery = function(option) {
 								$(settings.right).css("visibility","visible");
 								}
 								//　画像枚数表示
-								$("span.current").text(nowNumber);
+								settings.onChange(nowNumber);
+								console.log(nowNumber)
 								// ボーダー変更
 								var borderNum = parseInt(mLeft) / -liWidth;
 								$(settings.thum).find("li img").removeClass("select");
 								$(settings.thum).find("li img").eq(borderNum).addClass("select");
 							}
 						   });
-
 			   }
 			// nextボタンクリック
 			$(settings.right).click(function(){
@@ -94,9 +98,8 @@ $.fn.gallery = function(option) {
 			
 			// サムネイルクリック
 			$(settings.thum).find("li img").click(function(){
-				var Num = $(this).attr("deta-img");
-				changeTo(Num);
-				return false;
+										var direction = $(this).attr("deta-img");
+										changeTo(direction);
 			});
 	return this;
 };
