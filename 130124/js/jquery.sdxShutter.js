af3fc107bdb782de1f249af04dbdad1c6feba874
sunrise-fukuda <fukuda@sunrisedigital.jp>
2013-01-25 14:06:18 +0900

    更新

--- 130124/js/jquery.sdxShutter.js
+++ 130124/js/jquery.sdxShutter.js
@@ -2,18 +2,20 @@
 $.fn.shutter = function(option) {
 		   var settings = $.extend({
 					menu: null,
-					closeBtn: null
+					closeBtn: null,
+					mainBody:null
 	       }, option);
 
 	var opnBtn = $(this);
 
 	$(opnBtn).click(function(){
 		$(settings.menu).css("display","block");
+		$(settings.mainBody).css("display", "none");
 
 	});
 	$(settings.closeBtn).click(function(){
 		$(settings.menu).css("display","none");
-
+		$(settings.mainBody).css("display", "block");
 	});
 
 
